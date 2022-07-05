ae4d0171589bdbd9a52ead8d09c1d65a6ce50c7e
wxydejoy <1798923421@qq.com>
2022-07-05 20:40:11 +0800

    初试 有bug

--- main/tcp_client.c
+++ main/tcp_client.c
@@ -24,9 +24,10 @@
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 
+#include "driver/uart.h"
+#include "driver/gpio.h"
 
-
-//TCP/WIFI
+// TCP/WIFI
 #if defined(CONFIG_EXAMPLE_IPV4)
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
 #elif defined(CONFIG_EXAMPLE_IPV6)
@@ -37,9 +38,20 @@
 
 #define PORT CONFIG_EXAMPLE_PORT
 
+// UART
+#define ECHO_TEST_TXD 4
+#define ECHO_TEST_RXD 5
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM 2
+#define ECHO_UART_BAUD_RATE 115200
+#define ECHO_TASK_STACK_SIZE 2048
+
+#define BUF_SIZE (1024)
 
 static const char *TAG = "wxy";
-static const char *payload = "Message from ESP32 ";
+// static const char *payload = "Message from ESP32 ";
 
 static void tcp_client_task(void *pvParameters)
 {
@@ -48,7 +60,8 @@ static void tcp_client_task(void *pvParameters)
     int addr_family = 0;
     int ip_protocol = 0;
 
-    while (1) {
+    while (1)
+    {
 #if defined(CONFIG_EXAMPLE_IPV4)
         struct sockaddr_in dest_addr;
         dest_addr.sin_addr.s_addr = inet_addr(host_ip);
@@ -57,7 +70,7 @@ static void tcp_client_task(void *pvParameters)
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
 #elif defined(CONFIG_EXAMPLE_IPV6)
-        struct sockaddr_in6 dest_addr = { 0 };
+        struct sockaddr_in6 dest_addr = {0};
         inet6_aton(host_ip, &dest_addr.sin6_addr);
         dest_addr.sin6_family = AF_INET6;
         dest_addr.sin6_port = htons(PORT);
@@ -65,47 +78,91 @@ static void tcp_client_task(void *pvParameters)
         addr_family = AF_INET6;
         ip_protocol = IPPROTO_IPV6;
 #elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
-        struct sockaddr_storage dest_addr = { 0 };
+        struct sockaddr_storage dest_addr = {0};
         ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_STREAM, &ip_protocol, &addr_family, &dest_addr));
 #endif
-        int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
-        if (sock < 0) {
+        int sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+        if (sock < 0)
+        {
             ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
             break;
         }
         ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
 
         int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in6));
-        if (err != 0) {
+        if (err != 0)
+        {
             ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
             break;
         }
         ESP_LOGI(TAG, "Successfully connected");
 
-        while (1) {
-            int err = send(sock, payload, strlen(payload), 0);
-            if (err < 0) {
-                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+        ESP_LOGI(TAG, "Creat uart");
+
+        uart_config_t uart_config = {
+            .baud_rate = ECHO_UART_BAUD_RATE,
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+            .source_clk = UART_SCLK_APB,
+        };
+        int intr_alloc_flags = 1;
+        ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+        ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+        ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+
+        uint8_t *uart_data = (uint8_t *)malloc(BUF_SIZE);
+
+        ESP_LOGI(TAG, "Successfully created uart");
+
+        while (1)
+        {
+
+            int uart_len = uart_read_bytes(ECHO_UART_PORT_NUM, uart_data, BUF_SIZE, 20 / portTICK_RATE_MS);
+            if (uart_len < 0)
+            {
+                ESP_LOGE(TAG, "uart recv failed");
                 break;
             }
+            // Data received
+            if (uart_len > 0)
+            {
+                ESP_LOGI(TAG, "Received %d bytes from uart:", uart_len);
+                ESP_LOGI(TAG, "%s", uart_data);
+                int err = send(sock, uart_data, uart_len, 0);
+                // int err = send(sock, payload, strlen(payload), 0);
 
-            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+                if (err < 0)
+                {
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                    break;
+                }
+            }
+
+            int socket_len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0); //-1是因为要把'\0'加进去
             // Error occurred during receiving
-            if (len < 0) {
+            if (socket_len < 0)
+            {
                 ESP_LOGE(TAG, "recv failed: errno %d", errno);
                 break;
             }
             // Data received
-            else {
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+            else
+            {
+                rx_buffer[socket_len] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "Received %d bytes from %s:", socket_len, host_ip);
                 ESP_LOGI(TAG, "%s", rx_buffer);
+
+                uart_write_bytes(ECHO_UART_PORT_NUM, rx_buffer, strlen(rx_buffer));
             }
 
-            vTaskDelay(2000 / portTICK_PERIOD_MS);
+            //vTaskDelay(2000 / portTICK_PERIOD_MS);
         }
+        //free(uart_data);
 
-        if (sock != -1) {
+        if (sock != -1)
+        {
             ESP_LOGE(TAG, "Shutting down socket and restarting...");
             shutdown(sock, 0);
             close(sock);
@@ -114,16 +171,10 @@ static void tcp_client_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 
-
-
-
-
-
-
 void app_main(void)
 {
 
-    //wifi
+    // wifi
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -134,20 +185,5 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
 
-
-    
-
     xTaskCreate(tcp_client_task, "tcp_client", 4096, NULL, 5, NULL);
-
-
-
-
-
-
-
-
-
-    
 }
-
-
